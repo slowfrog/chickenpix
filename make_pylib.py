434bf1994f47c17a4d7fba956df818c1ee13099d
SlowFrog <laurentvaucher@gmail.com>
2012-07-02 17:58:01 +0100

    Added some Python libaries to the packaging script

--- make_pylib.py
+++ make_pylib.py
@@ -6,9 +6,23 @@ libpath = "K:/soft/Python27/lib"
 if __name__ == "__main__":
     out = zipfile.PyZipFile("python27.zip", "w")
     #out.writepy("toto.py")
-    for pack in ["os", 
-	             "ntpath", "stat", "genericpath", "warnings", "linecache", "types", "UserDict", 
-				 "_abcoll", "abc", "_weakrefset", "copy_reg" ]:
+    for pack in [
+        "UserDict",
+        "__future__",
+        "_abcoll",
+        "_weakrefset",
+        "abc",
+        "copy_reg",
+        "genericpath",
+        "hashlib",
+        "linecache",
+        "ntpath",
+        "os",
+        "random",
+        "stat",
+        "types",
+        "warnings"
+        ]:
         out.writepy(os.path.join(libpath, pack + ".py"))
     out.close()
 
